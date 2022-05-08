aea3cf266d8f4f59ee8be532da1a59bab941fa95
WhitePhosphorus4 <xionghao0613@163.com>
2022-05-08 14:39:53 +0800

    图核注意力机制测试

--- layers.py
+++ layers.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+from gettext import NullTranslations
 import numpy as np
 import torch
 import torch.nn as nn
@@ -8,46 +10,71 @@ class GraphAttentionLayer(nn.Module):
     """
     Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
     """
-    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True, kernel=NULL, sigma=1):
         super(GraphAttentionLayer, self).__init__()
         self.dropout = dropout
         self.in_features = in_features
         self.out_features = out_features
-        self.alpha = alpha
-        self.concat = concat
+        self.alpha = alpha  # leakyrelu的激活斜率
+        self.concat = concat    # 用于判断是否只有一个attention head
+        self.kernel = kernel    # 用于判断是否使用核注意力
+        self.sigma = sigma     # 核注意力的sigma
 
-        self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))
+        self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))    # 建立一个权重，用于对特征数F进行线性变换
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
-        self.a = nn.Parameter(torch.empty(size=(2*out_features, 1)))
+        self.a = nn.Parameter(torch.empty(size=(2*out_features, 1)))    # 计算α，输入是上一层两个输出的拼接，输出是eij
         nn.init.xavier_uniform_(self.a.data, gain=1.414)
 
-        self.leakyrelu = nn.LeakyReLU(self.alpha)
+        self.leakyrelu = nn.LeakyReLU(self.alpha)   # 激活
 
     def forward(self, h, adj):
         Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
-        e = self._prepare_attentional_mechanism_input(Wh)
+        e = self._prepare_attentional_mechanism_input(Wh)   # 计算注意力权重
 
         zero_vec = -9e15*torch.ones_like(e)
-        attention = torch.where(adj > 0, e, zero_vec)
+        attention = torch.where(adj > 0, e, zero_vec)   # 对于邻接矩阵中的元素，如果大于0，则说明有新的邻接边出现，那么则使用新计算出的权值，否则表示没有变连接，使用一个默认值来表示
         attention = F.softmax(attention, dim=1)
         attention = F.dropout(attention, self.dropout, training=self.training)
+        
         h_prime = torch.matmul(attention, Wh)
 
         if self.concat:
-            return F.elu(h_prime)
+            return F.elu(h_prime)   # 做一次激活
         else:
             return h_prime
 
     def _prepare_attentional_mechanism_input(self, Wh):
+
+        if not self.kernel:
+            e = _self_attentional_mechanism(Wh)
+        else:
+            e = _kernel_attentional_mechanism(Wh)
+            
+        # broadcast add
+        return self.leakyrelu(e)
+
+    def _self_attentional_mechanism(self, Wh):
         # Wh.shape (N, out_feature)
         # self.a.shape (2 * out_feature, 1)
         # Wh1&2.shape (N, 1)
         # e.shape (N, N)
         Wh1 = torch.matmul(Wh, self.a[:self.out_features, :])
         Wh2 = torch.matmul(Wh, self.a[self.out_features:, :])
+
         # broadcast add
         e = Wh1 + Wh2.T
-        return self.leakyrelu(e)
+        return e
+
+    def _kernel_attentional_mechanism(self, Wh):
+        # TODO: 实现核注意力
+        # kernel attentional mechanism
+        X = Wh
+        Y = Wh
+        D2 = np.sum(X*X, axis=1, keepdims=True) \
+            + np.sum(Y*Y, axis=1, keepdims=True).T \
+            - 2 * np.dot(X, Y.T)
+        e = np.exp(-D2 / (2 * self.sigma ** 2))
+        return e
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
